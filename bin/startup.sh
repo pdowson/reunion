db2c6c012205278c8909e0c490d6f8b270887d52
Phil Dowson <pjdowson@gmail.com>
2019-03-16 11:47:50 -0500

    Moved the cache warmup to be before migrations

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,10 +2,10 @@
 
 BASE_DIR=/var/www/html
 
+php ${BASE_DIR}/bin/console cache:warmup --env=prod --no-interaction
+
 php ${BASE_DIR}/bin/console doctrine:migrations:migrate --env=prod --no-interaction
 
 php ${BASE_DIR}/bin/console assets:install --env=prod --no-interaction
-
-php ${BASE_DIR}/bin/console cache:warmup --env=prod --no-interaction
 
 apache2-foreground
