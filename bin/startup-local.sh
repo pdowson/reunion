18863ea2fa4af7a3d86e537a880b91694c231e83
Phil Dowson <pjdowson@gmail.com>
2019-05-05 12:31:33 -0500

    Build optimizations

--- bin/startup-local.sh
+++ bin/startup-local.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Set the initial pid ID number
+pid=0
+
+# Handler for the term signal we are listening for
+term_handler() {
+  if [[ ${pid} -ne 0 ]]; then
+    kill -SIGTERM ${pid}
+    wait ${pid}
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# Trap is used to catch any signal that is sent to the process by docker stop
+trap 'kill ${!}; term_handler' SIGTERM
+
 BASE_DIR=/var/www/html
 
 php ${BASE_DIR}/bin/console --env=dev cache:warmup --no-interaction
@@ -8,4 +23,14 @@ php ${BASE_DIR}/bin/console --env=dev doctrine:migrations:migrate --no-interacti
 
 php ${BASE_DIR}/bin/console --env=dev assets:install --symlink --no-interaction
 
-apache2-foreground
+# run the original php CMD to put the foreground apache in the background
+apache2-foreground &
+
+# Set the last running pid to be the one we got from the last run process
+pid="$!"
+
+# Keep looking at the /dev/null path until the process ends (wait)
+while true
+do
+  wait ${!}
+done
--- bin/startup.sh
+++ bin/startup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+# Set the initial pid ID number
+pid=0
+
+# Handler for the term signal we are listening for
+term_handler() {
+  if [[ ${pid} -ne 0 ]]; then
+    kill -SIGTERM ${pid}
+    wait ${pid}
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# Trap is used to catch any signal that is sent to the process by docker stop
+trap 'kill ${!}; term_handler' SIGTERM
+
 BASE_DIR=/var/www/html
+
+ln -s /var/www/data/data.db /var/www/html/var/data.db
+
+ln -s /var/www/data/images/ /var/www/html/public/images/
 
 php ${BASE_DIR}/bin/console cache:warmup --env=prod --no-interaction
 
@@ -8,4 +27,14 @@ php ${BASE_DIR}/bin/console doctrine:migrations:migrate --env=prod --no-interact
 
 php ${BASE_DIR}/bin/console assets:install --env=prod --no-interaction
 
-apache2-foreground
+# run the original php CMD to put the foreground apache in the background
+apache2-foreground &
+
+# Set the last running pid to be the one we got from the last run process
+pid="$!"
+
+# Keep looking at the /dev/null path until the process ends (wait)
+while true
+do
+  wait ${!}
+done
